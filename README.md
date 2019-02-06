3c0ba7818d6516f307445316530394f02adaee4f
Elnaz Mojahedi <mojahedi.elnaz@gmail.com>
2019-02-06 01:00:02 +0100

    Initial commit - Android project boilerplate

---
+++ README.md
@@ -0,0 +1,3 @@
+[![Build Status](https://travis-ci.org/emojahedi/wikinews.svg?branch=master)](https://travis-ci.org/emojahedi/wikinews)
+
+# Wikinews Android App
---
+++ app/src/main/java/ir/teeleh/elnaz/wikinews/util/schedulers/BaseSchedulerProvider.java
@@ -0,0 +1,20 @@
+package ir.teeleh.elnaz.wikinews.util.schedulers;
+
+import android.support.annotation.NonNull;
+
+import io.reactivex.Scheduler;
+
+/**
+ * Allow providing different types of {@link Scheduler}s.
+ */
+public interface BaseSchedulerProvider {
+
+    @NonNull
+    Scheduler computation();
+
+    @NonNull
+    Scheduler io();
+
+    @NonNull
+    Scheduler ui();
+}
